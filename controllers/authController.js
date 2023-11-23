19c91ac568be63b9a2f24c93ae50158f0e9bc212
ArmanGolub <armanrahimov1024@gmail.com>
2023-11-23 20:41:33 +0600

    fix: add changes

--- controllers/authController.js
+++ controllers/authController.js
@@ -11,27 +11,28 @@
 const AuthService = require('./../services/authService');
 const asyncHandler = require('./../middlewares/asyncHandler');
 const ApiError = require('../exceptions/apiError');
+const UserDto = require('../dtos/userDto');
 
 
 exports.registration = asyncHandler(async (req, res, next) => {
     const requestUser = req.body;
     const userData = await AuthService.registration(requestUser);
     res.cookie('refreshToken', userData.refreshToken, { maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true })
-    return res.json(userData);
+    return res.json({ success: true, data: userData });
 })
 
 exports.login = asyncHandler(async (req, res,next) => {
     const requestUser = req.body;
     const userData = await AuthService.login(requestUser);
     res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true})
-    return res.json(userData);
+    return res.json({ success: true, data: userData });
 })
 
 exports.logout = asyncHandler(async (req, res, next) => {
     const {refreshToken} = req.cookies;
     const token = await AuthService.logout(refreshToken);
     res.clearCookie('refreshToken');
-    return res.json(token);
+    return res.json({ success: true, data: token });
 })
 
 exports.refresh = asyncHandler(async (req, res, next) => {
@@ -41,5 +42,13 @@ exports.refresh = asyncHandler(async (req, res, next) => {
         throw new ApiError(401, "invalid refresh")
     }
     res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true})
-    return res.json(userData);
+    return res.json({ success: true, data: userData });
 })
+
+exports.me = async (req, res) => {
+    const user = await User.findByPk(req.user.id);
+
+    const userData = new UserDto(user)
+  
+    res.status(200).json({ success: true, data: userData });
+  };
